eaf9104397e53d2afda6ac2087e26ccb3665b68d
Jakob <72307968+jakob30061@users.noreply.github.com>
2024-12-10 12:15:40 +0100

    feat: better structure

--- setup.py
+++ setup.py
@@ -4,8 +4,16 @@ import tensorflow as tf
 # Load dataset
 def setup_dataset(
         data_dir: str = "./dataset",
-        show_examples: bool = False
+        show_examples: bool = False,
+        labels = [0, 2, 77, 84, 119]
     ):
+    """
+    This function downloads the dataset and only keeps
+    the data specified in `labels`.  
+
+    It splitts the dataset in train and test dataset
+    """
+
     datasets, metadata = tfds.load(
         name="stanford_dogs",
         as_supervised=True,
@@ -14,11 +22,11 @@ def setup_dataset(
         split=['train[75%:]', 'test[:25%]'],
     )
 
-    # Filter for classes
-    allowed_labels = [0, 2, 77, 84, 119]
     train_dataset, test_dataset = list[tf.data.Dataset]([datasets[0], datasets[1]])
-    train_dataset = train_dataset.filter(lambda img, label: tf.reduce_any(tf.equal(label, allowed_labels)))
-    test_dataset = test_dataset.filter(lambda img, label: tf.reduce_any(tf.equal(label, allowed_labels)))
+
+    # Filter for classes
+    train_dataset = train_dataset.filter(lambda img, label: tf.reduce_any(tf.equal(label, labels)))
+    test_dataset = test_dataset.filter(lambda img, label: tf.reduce_any(tf.equal(label, labels)))
 
     # Show examples
     if show_examples:
